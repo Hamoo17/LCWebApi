6a1f8a5bbc9906c335dd8364ad9520104b4fa6c5
Ahmed Magdy <ahmedmagdy4122@gmail.com>
2022-03-08 16:37:35 +0200

    New Services Added

--- LCWebApi/Server/Controllers/SubscriptionController.cs
+++ LCWebApi/Server/Controllers/SubscriptionController.cs
@@ -10,7 +10,7 @@ using System.Data;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
- 
+
 using static LCWebApi.Shared.EnumsCls;
 
 namespace LCWebApi.Server.Controllers
@@ -26,24 +26,24 @@ namespace LCWebApi.Server.Controllers
             _context = context;
         }
         [HttpGet]
-        public async Task<ActionResult> GetCid(string phone) 
+        public async Task<ActionResult> GetCid(string phone)
         {
-            var result = await _context.CustomerPlans.Where(x=>x.Custphone.Contains(phone) || x.Custmobile.Contains(phone))
+            var result = await _context.CustomerPlans.Where(x => x.Custphone.Contains(phone) || x.Custmobile.Contains(phone))
                 .Select(x => new
                 {
                     CID = x.CustomerId,
                     PlanTitle = x.Custplan,
                     RemainingDays = x.SubscriptionId,
-                    Status = ((SubscriptionStatus)(x.SubscriptionId==0? 3: x.Isdeleted)).ToString(),
+                    Status = ((SubscriptionStatus)(x.SubscriptionId == 0 ? 3 : x.Isdeleted)).ToString(),
                 }).Distinct()
                 .ToListAsync();
 
-        return Ok(result);
+            return Ok(result);
         }
         [HttpGet]
         public ActionResult GetSubscription(int CID)
         {
-            var PlanDetails =  _context.CustomerPlans.Where(x => x.CustomerId == CID)
+            var PlanDetails = _context.CustomerPlans.Where(x => x.CustomerId == CID)
                 .Select(x => new
                 {
                     CID = x.CustomerId,
@@ -75,7 +75,7 @@ namespace LCWebApi.Server.Controllers
                         {
                             cmd.CommandText = $"exec PLANDays @CCID= {CID}";
                         }
-                       
+
                         //  cmd.CommandType = CommandType.StoredProcedure;
                         //  cmd.Parameters.Add(new SqlParameter("@CustomerID", CustomerID));
                         using (var reader = cmd.ExecuteReader())
@@ -88,7 +88,7 @@ namespace LCWebApi.Server.Controllers
                 {
                     if (connectionState != ConnectionState.Closed) conn.Close();
                 }
-                 
+
                 dt.DefaultView.Sort = "dayName ASC";
                 dt = dt.DefaultView.ToTable();
                 Func<dynamic, object> selector = PlanDetails.Status == "ACTIVE" ? x => x.DeliveryDate : x => x.dayid;
@@ -96,11 +96,11 @@ namespace LCWebApi.Server.Controllers
                 {
                     PlanInfo = PlanDetails,
                     Columns = dt.GetCulomnsName(),
-                     PlanDetails =dt.ToDynamic().OrderBy(selector)
-                });     
+                    PlanDetails = dt.ToDynamic().OrderBy(selector)
+                });
             }
 
-            
+
         }
         [HttpGet]
         public IActionResult GetMainPlan(int CID)
@@ -129,7 +129,7 @@ namespace LCWebApi.Server.Controllers
                     if (connectionState != ConnectionState.Open) conn.Open();
                     using (var cmd = conn.CreateCommand())
                     {
-                            cmd.CommandText = $"exec PLANDays @CCID= {CID}";
+                        cmd.CommandText = $"exec PLANDays @CCID= {CID}";
                         using (var reader = cmd.ExecuteReader())
                         {
                             dt.Load(reader);
@@ -140,19 +140,168 @@ namespace LCWebApi.Server.Controllers
                 {
                     if (connectionState != ConnectionState.Closed) conn.Close();
                 }
-                
+
                 dt.DefaultView.Sort = "dayName ASC";
                 dt = dt.DefaultView.ToTable();
-               
+
 
                 return Ok(new
                 {
                     PlanInfo = PlanDetails,
                     Columns = dt.GetCulomnsName(),
                     PlanDetails = dt.ToDynamic().OrderBy(x => x.dayid),
-                   
+
                 });
             }
+
+        }
+        [HttpGet]
+        public IActionResult GetMealsPlanNutiration(int CID)
+        {
+            var Nutirations = (from CustomerPlan in _context.CustomerPlans
+                               where CustomerPlan.CustomerId == CID
+                               join Meal in _context.Meals on CustomerPlan.MealId equals Meal.MealId
+                               join dayName in _context.Kitday2s.Take(28) on CustomerPlan.DayId equals dayName.Did
+                               orderby CustomerPlan.DayId
+                               select new
+                               {
+
+                                   CustomerID = CustomerPlan.CustomerId,
+                                   DayID = CustomerPlan.DayId,
+                                   Day = dayName.Dname,
+                                   Meals =
+                                           new
+                                           {
+                                               MealName = Meal.MealName,
+                                               Calories = Meal.Calories,
+                                               Protiens = Meal.Protiens,
+                                               Fats = Meal.Fats,
+                                               Carb = Meal.Carb
+                                           }
+
+                               }
+
+                               ).ToList();
+            var Max = Nutirations.Max(x => x.DayID);
+            if (Max >= 15)
+            {
+                return Ok(new
+                {
+                    Week1 = Nutirations.Where(x => x.DayID <= 7),
+                    Week2 = Nutirations.Where(x => x.DayID > 7 && x.DayID <= 14),
+                    Week3 = Nutirations.Where(x => x.DayID >= 15 && x.DayID <= 21),
+                    Week4 = Nutirations.Where(x => x.DayID >= 22)
+                });
+            }
+            else
+            {
+                return Ok(new
+                {
+                    Week1 = Nutirations.Where(x => x.DayID <= 7),
+                    Week2 = Nutirations.Where(x => x.DayID > 7)
+                });
+            }
+
+
+        }
+        [HttpGet]
+        public IActionResult GetNutirationAVG(int CID)
+        {
+            var Nutirations = (from CustomerPlan in _context.CustomerPlans
+                               where CustomerPlan.CustomerId == CID
+                               join Meal in _context.Meals on CustomerPlan.MealId equals Meal.MealId
+                               join dayName in _context.Kitday2s.Take(28) on CustomerPlan.DayId equals dayName.Did
+                               orderby CustomerPlan.DayId
+                               select new
+                               {
+
+                                   CustomerID = CustomerPlan.CustomerId,
+                                   DayID = CustomerPlan.DayId,
+                                   Day = dayName.Dname,
+                                   Meals =
+                                           new
+                                           {
+                                               MealName = Meal.MealName,
+                                               Calories = Meal.Calories,
+                                               Protiens = Meal.Protiens,
+                                               Fats = Meal.Fats,
+                                               Carb = Meal.Carb
+                                           }
+
+                               }
+
+                               ).ToList();
+
+            var Max = Nutirations.Max(x => x.DayID);
+            var PlanDays = Nutirations.GroupBy(x => x.DayID).Distinct().ToList();
+            if (Max >= 15)
+            {
+                var AVG = PlanDays.Count / 4;
+                return Ok(new
+                {
+                    Week1 =
+                    new
+                    {
+                        Calories = Math.Floor((decimal)(Nutirations.Where(x => x.DayID <= 7).Sum(x => x.Meals.Calories) / AVG)),
+                        Protiens = Math.Floor((decimal)(Nutirations.Where(x => x.DayID <= 7).Sum(x => x.Meals.Protiens) / AVG)),
+                        Fats = Math.Floor((decimal)(Nutirations.Where(x => x.DayID <= 7).Sum(x => x.Meals.Fats) / AVG)),
+                        Carb = Math.Floor((decimal)(Nutirations.Where(x => x.DayID <= 7).Sum(x => x.Meals.Carb) / AVG)),
+                    }
+                    ,
+
+                    Week2 =
+                    new
+                    {
+                        Calories = Math.Floor((decimal)(Nutirations.Where(x => x.DayID > 7 && x.DayID <= 14).Sum(x => x.Meals.Calories) / AVG)),
+                        Protiens = Math.Floor((decimal)(Nutirations.Where(x => x.DayID > 7 && x.DayID <= 14).Sum(x => x.Meals.Protiens) / AVG)),
+                        Fats = Math.Floor((decimal)(Nutirations.Where(x => x.DayID > 7 && x.DayID <= 14).Sum(x => x.Meals.Fats) / AVG)),
+                        Carb = Math.Floor((decimal)(Nutirations.Where(x => x.DayID > 7 && x.DayID <= 14).Sum(x => x.Meals.Carb) / AVG)),
+                    }
+                    ,
+
+                    Week3 = new
+                    {
+                        Calories = Math.Floor((decimal)(Nutirations.Where(x => x.DayID >= 15 && x.DayID <= 21).Sum(x => x.Meals.Calories) / AVG)),
+                        Protiens = Math.Floor((decimal)(Nutirations.Where(x => x.DayID >= 15 && x.DayID <= 21).Sum(x => x.Meals.Protiens) / AVG)),
+                        Fats = Math.Floor((decimal)(Nutirations.Where(x => x.DayID >= 15 && x.DayID <= 21).Sum(x => x.Meals.Fats) / AVG)),
+                        Carb = Math.Floor((decimal)(Nutirations.Where(x => x.DayID >= 15 && x.DayID <= 21).Sum(x => x.Meals.Carb) / AVG)),
+                    }
+                    ,
+
+                    Week4 = new
+                    {
+                        Calories = Math.Floor((decimal)(Nutirations.Where(x => x.DayID >= 22).Sum(x => x.Meals.Calories) / AVG)),
+                        Protiens = Math.Floor((decimal)(Nutirations.Where(x => x.DayID >= 22).Sum(x => x.Meals.Protiens) / AVG)),
+                        Fats = Math.Floor((decimal)(Nutirations.Where(x => x.DayID >= 22).Sum(x => x.Meals.Fats) / AVG)),
+                        Carb = Math.Floor((decimal)(Nutirations.Where(x => x.DayID >= 22).Sum(x => x.Meals.Carb) / AVG)),
+                    }
+
+                });
+            }
+            else
+            {
+                var AVG = PlanDays.Count / 2;
+                return Ok(new
+                {
+                    Week1 =
+                    new
+                    {
+                        Calories = Math.Floor((decimal)(Nutirations.Where(x => x.DayID <= 7).Sum(x => x.Meals.Calories) / AVG)),
+                        Protiens = Math.Floor((decimal)(Nutirations.Where(x => x.DayID <= 7).Sum(x => x.Meals.Protiens) / AVG)),
+                        Fats = Math.Floor((decimal)(Nutirations.Where(x => x.DayID <= 7).Sum(x => x.Meals.Fats) / AVG)),
+                        Carb = Math.Floor((decimal)(Nutirations.Where(x => x.DayID <= 7).Sum(x => x.Meals.Carb) / AVG)),
+                    },
+                    Week2 = new
+                    {
+                        Calories = Math.Floor((decimal)(Nutirations.Where(x => x.DayID > 7).Sum(x => x.Meals.Calories) / AVG)),
+                        Protiens = Math.Floor((decimal)(Nutirations.Where(x => x.DayID > 7).Sum(x => x.Meals.Protiens) / AVG)),
+                        Fats = Math.Floor((decimal)(Nutirations.Where(x => x.DayID > 7).Sum(x => x.Meals.Fats) / AVG)),
+                        Carb = Math.Floor((decimal)(Nutirations.Where(x => x.DayID > 7).Sum(x => x.Meals.Carb) / AVG)),
+                    }
+
+                });
+            }
+
 
         }
     }
